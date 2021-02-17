e329e33c83e6f05e79cf7747b725fece4f555715
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2021-02-17 17:12:53 +0100

    spacing

--- lib/util/chunker.ex
+++ lib/util/chunker.ex
@@ -4,8 +4,8 @@ defmodule Mandelbrot.Util.Chunker do
   defp do_chunk(_, 0, chunks), do: chunks
 
   defp do_chunk(to_chunk, parts, chunks) do
-  chunk_length = to_chunk |> length() |> div(parts)
-  {chunk, rest} = Enum.split(to_chunk, chunk_length)
-  do_chunk(rest, parts - 1, [chunk | chunks])
+    chunk_length = to_chunk |> length() |> div(parts)
+    {chunk, rest} = Enum.split(to_chunk, chunk_length)
+    do_chunk(rest, parts - 1, [chunk | chunks])
   end
 end
